24ec979d998c06717d44a8237bef1c46372ed38e
MaximZhelev <maxim.zhelev1@gmail.com>
2022-09-20 10:59:28 +0300

    Fix deployment

--- src/components/projects/live-data.tsx
+++ src/components/projects/live-data.tsx
@@ -1,24 +1,9 @@
 import * as React from 'react';
-// import StackGrid from 'react-stack-grid';
-import { Box, useMediaQuery } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import { PageSlideFade } from '../ui/page-transitions';
 import RepositoryCard from './live-data-card';
 
 const LiveData = ({ repositories }) => {
-  const [isLargerThan720] = useMediaQuery('(min-width: 720px)');
-  const [isLargerThan982] = useMediaQuery('(min-width: 982px)');
-
-  let columnWidth = 390;
-  if (isLargerThan982) {
-    columnWidth = 390;
-  } else {
-    if (isLargerThan720) {
-      columnWidth = 300;
-    } else {
-      // columnWidth = '100%';
-    }
-  }
-
   return (
     <PageSlideFade>
       <Box mt={4}>
